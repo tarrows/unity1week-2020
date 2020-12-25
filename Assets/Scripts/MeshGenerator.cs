421d2125c25b66e8f95281806132a1fd3751cd68
tarrows <rem_tate@yahoo.co.jp>
2020-12-25 12:20:05 +0900

    Generate mesh from script

---
+++ Assets/Scripts/MeshGenerator.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class MeshGenerator : MonoBehaviour
+{
+    Mesh mesh;
+    [ContextMenu("Generate Mesh")]
+    private void generate() {
+        mesh = new Mesh();
+        mesh.Clear();
+
+        var vertices = (
+            from y in new[] { -1, 1 }
+            from x in new[] { -1, 1 }
+            select new Vector3(x, y, 0)
+        ).ToArray();
+
+        Debug.Log(string.Join(", ", vertices));
+        mesh.vertices = vertices;
+
+        mesh.triangles = new[] { 0, 1, 2, 2, 1, 3 };
+
+        mesh.RecalculateBounds();
+        mesh.RecalculateNormals();
+
+        GetComponent<MeshFilter>().sharedMesh = mesh;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
